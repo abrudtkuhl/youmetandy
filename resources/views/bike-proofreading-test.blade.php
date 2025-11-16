a89e6e6e80a0e0f83373003f75ad685ed74e0199
Andy Brudtkuhl <andy@brudtkuhl.com>
2025-11-16 13:29:30 -0600

    spell checker test

---
+++ resources/views/bike-proofreading-test.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Bike Proofreeding Test Page</title>
+</head>
+<body>
+    <h1>Bike Riding Proofreeding Test</h1>
+
+    <p>
+        I realy like rideing my bike in the mornings, when the air feels kind of crisp and the streets are almost completly quiet. 
+        Sometimes I start out too fast and my legs gets tired quicker then I expected, but I just keep peddling anyways beacuse it feels 
+        good to be outside. The sun is usualy still low, casting long shaddows from the trees that line the rode and the houses with there small yards.
+    </p>
+
+    <p>
+        On weekends me and my friend usualy meets at the corner and decide which way we are going to ride that day. 
+        We dont always agree about the best route, one of us wants the flattest possible path and the other one insists on 
+        climbing all the bigest hills for the sake of so called “training”. Sometimes we argue a bit, but in the end it doesnt 
+        realy matter witch way we go because we both end up exausted and kind of happy.
+    </p>
+
+    <p>
+        There is a long bike trail near by that used to be an old rail road track before the city turned it into a path. 
+        The gravel is mostly smooth but ocasionally theres loose stones and sticks laying around that makes the wheels wobble 
+        a little. When we pass other riders, some of them nods politly, others just looks straight ahead like they are very 
+        serious profesionals in the middle of a important race, even if they are actualy just going pretty slow.
+    </p>
+
+    <p>
+        One time I forgot to pump my tires and they was so soft that every bump felt like I was draging the whole bike through mud. 
+        I kept thinking to myself that I should of checked the tire preasure before we left, but by then it was already to late and we 
+        were far away from home. My friend said it was “good resistance training”, but I am prety sure he was just laughng at me on the inside.
+    </p>
+
+    <p>
+        Another day we decided to try rideing in the light rain, which sounds romantic but actualy gets pretty cold and soggy after a while. 
+        The water splashes up from the back wheel straight onto my jacket, leaving a long dirty stripe that never totaly comes out in the wash. 
+        My glasses fogs up, the brakes squeel, and the chain makes a creeking noise every time I shift gears too quickly. Still, there is 
+        something strangely relaxing about listening to the soft hiss of the tires rolling over the wet pavement.
+    </p>
+
+    <p>
+        When I first learned to ride, I was so nervous that I kept staring down at the front wheel instead of looking ahead of me. 
+        Because of that I would swerve all over the place and almost ran into the mail box a couple times. My dad kept saying, 
+        “look where you want to go, not where you are afraid you might crash at”, which sounded like a confusing philosofy lesson 
+        but eventually started to make sense. Once I finaly learned to trust my balance, the whole thing felt way less scary and much more funner.
+    </p>
+
+    <p>
+        Now I also pay more atention to the sounds around me while I am rideing. In the park you can hear little birds chirping, 
+        kids shouting in the playgroung, and dogs barking randomness from behind fences. Sometimes a car rushes by too close and the 
+        whoosh of air makes me grip the handle bars harder then necesary. Cars are supposed to give bikes enough room but they dont always 
+        do it, so I try to stay as visible as posible even if it makes me look slightly ridiculous with all the bright colors.
+    </p>
+
+    <p>
+        I have been thinking about getting a new bike, maybe one of those fancy gravel ones with disc breaks and extra wide tires. 
+        The problem is that I am easily distracted by shiny gear and might spend more time reserching specs then actualy riding. 
+        There is always some new component that claims to be lighter, stronger or more erodynamic then last years model. 
+        If I am honest though, the biggest improvment I could make would probably just be pedaling more consistantly and not eating 
+        so many snacks right before we go out.
+    </p>
+
+    <p>
+        In the end, riding bikes isnt really about having the perfect equiptment or the most optimised training plan. 
+        It is more about the weird little moments, like when you suddenly smells fresh bread from a bakery you didnt even knew was there, 
+        or when a random stranger shouts “nice bike!” as they cross the street. Even when the weather is wrong, the seat feels slightly crooked, 
+        and my legs complane the entire time, I still usualy feel better after a ride then before it. That is probably why I keep going out, 
+        even on days where staying home sounds more sensible on paper.
+    </p>
+</body>
+</html>
+
+
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/bike-proofreading-test', function () {
+    return view('bike-proofreading-test');
+});
